bf6f99c0b9b5e8e6b03246d56a3dbee2698a4f50
gwen-kaser <69249726+gwen-kaser@users.noreply.github.com>
2021-10-19 13:30:19 +0200

    Initial commit

---
+++ view/frontend/listDestinationView.php
@@ -0,0 +1,35 @@
+<?php $title = 'Nuit insolite/accueil'; ?>
+
+    <!--Accueil-->
+    <div class="img" style="background: url(Images/home.jpg) no-repeat center center fixed; background-size: cover; height: 100vh;" alt="Paysage insolite;">
+        <div class="logo text-center py-5">
+            <img class="img-fluid" src="Images/logo.png" alt>
+        </div>
+    </div>
+
+    <!--Destinations-->
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col">
+                <div class="jumbotron text-center">
+                    <h2>Choisissez la destination de vos rêves<br/>
+                        pour une nuit que vous n'êtes pas prêt d'oublier !</h2>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row mb-5">
+            <div class="col-md-4 offset-md-2">
+                <img class="img-fluid" src="Images/borabora-pilotis-palace-1.jpg" alt>
+            </div>
+            <div class="col-md-5">
+                <div class="text-block">
+                    <h3>BORABORA, Pilotis Palace</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis laudantium at debitis veniam recusandae ipsam saepe fugit qui!</p>
+                    <button type="submit">Découvrir</button>
+                </div>
+            </div>
+          </div>
+          <hr>
+
+<?php require('template.php'); ?>
