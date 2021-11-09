b5dc6f89593d87c247751e422e81a26d9f7c2403
gwen-kaser <69249726+gwen-kaser@users.noreply.github.com>
2021-11-09 13:45:13 +0100

    Modification code

--- controller/Website.php
+++ controller/Website.php
@@ -3,8 +3,15 @@
 class Website 
 {
     // Afficher page d'accueil
-    public function home () {
-
+    public function home () 
+    {
         require ('view/frontend/listDestinationsView.php');
     }
+
+    // Afficher page destination
+    public function destination ()
+    {
+        require ('view/frontend/destinationView.php');
+    }
+    
 }
