ff02b8c59fe824831a585297e49c6d337527dcd0
gwen-kaser <69249726+gwen-kaser@users.noreply.github.com>
2021-11-23 14:12:56 +0100

    Création page Admin - Gestion des destinations

--- view/frontend/listDestinationsAdmin.php
+++ view/frontend/listDestinationsAdmin.php
@@ -2,8 +2,16 @@
 
 <?php ob_start(); ?>
 
+    <div class="jumbotron jumbotron-fluid" style="background: url(public/images/home.jpg) no-repeat center center fixed; background-size: cover;">
+        <div class="container py-5 text-center">
+            <img class="img-fluid mb-5" src="public/images/logo.png" alt>
+        </div>
+    </div>
 
 
+    <div class="text-center py-5">
+        <a href="index.php?action=addDestinationView" class="btn btn-sm border border-primary active" role="button" aria-pressed="true">Ajouter une destination</a>
+    </div>
 
 
 
