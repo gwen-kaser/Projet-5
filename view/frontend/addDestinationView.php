7738d179c139d482a82b8ddf48449b52bdaf321a
gwen-kaser <69249726+gwen-kaser@users.noreply.github.com>
2021-11-23 14:15:51 +0100

    Ajout type File - L'envoi de fichier

--- view/frontend/addDestinationView.php
+++ view/frontend/addDestinationView.php
@@ -8,30 +8,30 @@
             <div class="col-12 col-md-6 col-lg-4 py-5">
                 <h3 class="pb-4">NOUVELLE DESTINATION</h3>
                 
-                <form action="index.php?action=addDestination" method="post">
+                <form action="index.php?action=addDestination" method="post" enctype="multipart/form-data">
                     <div class="form-group">
-                        <input type="text" id="title" name="title" required="">
                         <label for="title">TITRE</label><br/>
+                        <input type="text" id="title" name="title" required="">
                     </div>
                     <div class="form-group">
-                        <input type="text" id="content" name="content" required="">
                         <label for="content">TEXTE</label><br/>
+                        <input type="text" id="content" name="content" required="">
                     </div>
                     <div class="form-group">
-                        <input type="text" id="image_home" name="image_home" required="">
                         <label for="image_home">IMAGE PAGE D'ACCUEIL</label><br/>
+                        <input type="file" class="form-control" id="image_home" name="image_home" required="">
                     </div>
                     <div class="form-group">
-                        <input type="text" id="image" name="image" required="">
                         <label for="image">IMAGE</label><br/>
+                        <input type="file" class="form-control" id="image" name="image" required="">
                     </div>
                     <div class="form-group">
-                        <input type="text" id="latitude" name="latitude" required="">
                         <label for="latitude">LATITUDE</label><br/>
+                        <input type="text" id="latitude" name="latitude" required="">
                     </div>
                     <div class="form-group">
-                        <input type="text" id="longitude" name="longitude" required="">
                         <label for="longitude">LONGITUDE</label><br/>
+                        <input type="text" id="longitude" name="longitude" required="">
                     </div>
                     <button type="submit" class="btn-primary btn-sm">CONFIRMER</button>
                 </form>
