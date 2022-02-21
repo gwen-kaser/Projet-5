712e4510b94c32716891add43201028481b686a7
gwen-kaser <69249726+gwen-kaser@users.noreply.github.com>
2022-02-21 15:10:37 +0100

    Ajout Jumbotron

--- view/frontend/listDestinationsHome.php
+++ view/frontend/listDestinationsHome.php
@@ -9,6 +9,17 @@
         </div>
     </div>  
 
+    <div class="container text-center py-5">
+        <div class="jumbotron bg-light border border-primary border border-top-0 border border-end-0 shadow">
+            <h3 class="font-weight-light font-italic">NUITS INSOLITES</h3>
+            <p class="font-italic">
+            Vous propose plusieurs destinations avec des séjours INSOLITES
+            que vous n'êtes pret d'oublier !<br/>
+            Pour les découvrir, Connectez-vous ou inscrivez-vous pour tout savoir sur ces hôtels qui vous feront rêver !
+            </p> 
+        </div>
+    </div>
+
     <!--Affichage des destinations-->
     <?php
     while ($data = $destinations->fetch())
@@ -17,11 +28,15 @@
         <div class="container py-5">
             <div class="row mb-5">
                 <div class="col-md-4 offset-md-2">
-                    <img class="img-fluid" src="uploads/<?= ($data['image_slider']) ?>" alt>
+                    <img class="img-fluid" src="uploads/<?= ($data['image_slider']) ?>" alt="Nuits insolites">
                 </div>
                 <div class="col-md-5">
                     <div class="text-block">
                         <h4 class="font-weight-light"><?= htmlspecialchars($data['title']) ?></h4>
+                        <?php 
+                            if(strlen($data['content']) > 200) { 
+                            $data['content'] = substr($data['content'], 0, 200).'...'; } 
+                        ?>
                         <p><?= htmlspecialchars($data['content']) ?></p>
                         
                         <!-- Condition si le membre est connecté il peut accéder à la destination -->
